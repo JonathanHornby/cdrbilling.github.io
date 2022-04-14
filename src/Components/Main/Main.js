04418ee04adf6e9978a92d26b5f1ec374792ba14
JonathanHornby <jtorn9@gmail.com>
2022-04-14 17:00:35 +1000

    Setup Main window with main left and main right

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Main.css';
 
 // Main component acts as a container for the components which will display tabulated data in the window
 
@@ -7,8 +8,15 @@ class Main extends React.Component {
 
     render() {
         return (
-            <div className="Main">
-                Test Main
+            <div className='MainWindow'>
+                {/* MainLeft to display lists of customers / users / reports to select for display in MainRight */}
+                <div className="MainLeft">
+                    Test Main Left
+                </div>
+                {/* MainRight to display table data for the selected option in MainLeft */}
+                <div className="MainRight">
+                    Test Main Right
+                </div>
             </div>
         )
     }
